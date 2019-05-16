e0586caec00a1bcc06596dd710597792725e0477
Harry Hornbacher <harry.hornbacher@futurice.com>
2019-05-16 17:09:12 +0200

    add own usb peripheral parts

---
+++ donkeycar/parts/usbperipheral/parts.py
@@ -0,0 +1,70 @@
+from .protocol import Protocol, SensorPacket
+
+class SteeringControl:
+    def __init__(self, protocol: Protocol):
+        self._protocol = protocol
+
+    def run(self, steering):
+        self._protocol.setSteering(steering)
+
+class ThrottleControl:
+    def __init__(self, protocol: Protocol):
+        self._protocol = protocol
+
+    def run(self, throttle):
+        self._protocol.setThrottle(throttle)
+
+class IMUSensor:
+    def __init__(self, protocol: Protocol):
+        self._protocol = protocol
+        self.data = None
+
+    def run(self,):
+        return self.data
+
+class DistanceSensor:
+    def __init__(self, protocol: Protocol):
+        self._protocol = protocol
+        self.data = None
+
+    def run(self,):
+        return self.data
+
+class PeripheralPart:
+    def __init__(self):
+        self._protocol = Protocol(packetHandler=self._updateValues)
+        self._steering = SteeringControl(self._protocol)
+        self._throttle = ThrottleControl(self._protocol)
+        self._imu = IMUSensor(self._protocol)
+        self._distance = DistanceSensor(self._protocol)
+
+    def getSteeringPart(self):
+        return self._steering
+
+    def getThrottlePart(self):
+        return self._throttle
+
+    def getIMUPart(self):
+        return self._imu
+
+    def getDistancePart(self):
+        return self._distance
+
+    def _updateValues(self, packet:SensorPacket):
+        # self._current = packet
+        self._imu.data = {
+            'accel': packet.acceleration,
+            'gyro': packet.gyro,
+            'magneto': packet.magneto
+        }
+        self._distance.data = {
+            'left': packet.distance[0],
+            'right': packet.distance[1],
+            'center': packet.distance[2]
+        }
+
+    def update(self):
+        self._protocol.start()
+
+    def run_threaded(self,):
+        pass
---
+++ donkeycar/parts/usbperipheral/protocol.py
@@ -0,0 +1,104 @@
+# Sources from: https://tinkering.xyz/async-serial/
+# Thanks for the great article!
+
+import asyncio
+import serial_asyncio
+from sliplib import Driver as SLIPDriver
+from zlib import crc32
+import struct
+
+
+def calcChecksum(data):
+    return (crc32(data) & 0xffffffff).to_bytes(4, 'little')
+
+
+slipDriver = SLIPDriver()
+
+
+class SensorPacket:
+    def __init__(self, data):
+        payload = data[0:24]
+        checksum = data[24:28]
+        calculatedChecksum = calcChecksum(payload)
+
+        self.valid = checksum == calculatedChecksum
+        if not self.valid:
+            return
+
+        tmp = list(struct.unpack('<HHHHHHHHHHHH', payload))
+
+        self.distance = tmp[0:3]
+        self.magneto = tmp[3:6]
+        self.acceleration = tmp[6:9]
+        self.gyro = tmp[9:12]
+
+    def __str__(self):
+        if not self.valid:
+            return 'SensorMessage: Invalid!'
+        return 'SensorMessage:\nDist: {0}\nCompass: {1}\nAccel: {2}\nGyro: {3}'.format(
+            self.distance,
+            self.magneto,
+            self.acceleration,
+            self.gyro
+        )
+
+
+class Serial(asyncio.Protocol):
+    def __init__(self, packetHandler=lambda: None):
+        super().__init__()
+        self._packetHandler = packetHandler
+
+    def connection_made(self, transport):
+        self._transport = transport
+        self._buffer = bytearray()
+
+    def connection_lost(self, exc):
+        print('Writer closed')
+
+    async def send(self, packet):
+        self._transport.serial.write(packet)
+
+    def data_received(self, data):
+        print('Data received')
+        self._buffer += data
+        if len(self._buffer) >= 28:
+            try:
+                data = slipDriver.receive(self._buffer)
+            except:
+                pass
+            if len(data) > 0:
+                for rawPacket in data:
+                    packet = SensorPacket(rawPacket)
+                    if packet.valid:
+                        self._packetHandler(packet)
+
+
+class Protocol:
+    def __init__(self, device='/dev/ttyACM0', baud=115200, packetHandler=lambda: None):
+        self._loop = asyncio.get_event_loop()
+        self._serialProtocol = Serial(packetHandler)
+        self._serialConnection = serial_asyncio.create_serial_connection(
+            self._loop, lambda: self._serialProtocol, device, baudrate=baud)
+        asyncio.ensure_future(self._serialConnection)
+
+        self._steering = 0
+        self._throttle = 0
+
+    def start(self):
+        self._loop.run_forever()
+
+    def setSteering(self, steering):
+        self._steering = steering
+        self._sendControlPacket(self._steering, self._throttle)
+
+    def setThrottle(self, throttle):
+        self._throttle = throttle
+        self._sendControlPacket(self._steering, self._throttle)
+
+    def _sendControlPacket(self, steering, throttle):
+        print('Steering: {0}, Speed: {1}'.format(steering, throttle))
+        payload = bytearray(struct.pack('<HH', steering, throttle))
+        checksum = calcChecksum(payload)
+        payload.extend(checksum)
+        self._loop.create_task(
+            self._serialProtocol.send(slipDriver.send(payload)))
