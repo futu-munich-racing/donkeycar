628b969260759c57817bf5d13760df49e10f361e
Teemu Kinnunen <teemu.kinnune@futurice.com>
2019-11-21 22:35:59 +0200

    added input

--- preprocess.sh
+++ preprocess.sh
@@ -1,6 +1,10 @@
-!if [ ! -d "data/train" ];
-    then unzip -q -o "train.zip" -d data/train/;
+ls /valohai/inputs
+
+datadir=/valohai/inputs
+
+if [ ! -d "data/train" ];
+    then unzip -q -o "$datadir/train.zip" -d data/train/;
 fi
 !if [ ! -d "data/val" ];
-    then unzip -q -o "val.zip" -d data/val/;
+    then unzip -q -o "$datadir/val.zip" -d data/val/;
 fi
